156159b53526513d44d37278bfcf1292cb5f587e
Articunogem12 <adrifer1705@gmail.com>
2024-11-15 16:52:00 +0100

    Icona introduïda en la pantalla principal. Mirar color de background

--- app/src/main/java/com/example/m07uf1pr01tiagonavarrosergisaravia/MainActivity.kt
+++ app/src/main/java/com/example/m07uf1pr01tiagonavarrosergisaravia/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.m07uf1pr01tiagonavarrosergisaravia.ui.theme.M07UF1PR01TiagoNavarroSergiSaraviaTheme
 
@@ -32,10 +36,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(modifier = Modifier
+        . fillMaxWidth()){
+        Image(contentScale = ContentScale.FillWidth
+            , painter = painterResource(id = R.drawable.icono)
+            , contentDescription = "Descripció"
+            , modifier = Modifier
+                .fillMaxSize()
+        )
+    }
 }
 
 @Preview(showBackground = true)
