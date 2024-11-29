0ec4f92456e4587959f19030cda4253b7540f748
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2024-11-29 12:20:21 +0100

    error archivo routes resuelto e imports echos para entryPoint

--- app/src/main/java/com/example/pr04_ahorcado/entryPoint.kt
+++ app/src/main/java/com/example/pr04_ahorcado/entryPoint.kt
@@ -1,2 +1,10 @@
 package com.example.pr04_ahorcado
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.example.pr04_ahorcado.Routes
