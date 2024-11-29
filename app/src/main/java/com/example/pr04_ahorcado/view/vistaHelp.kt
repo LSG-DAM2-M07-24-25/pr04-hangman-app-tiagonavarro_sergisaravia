2fd23c11fb140604edb2cd564ef66c446ec2b4bb
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2024-11-29 12:05:09 +0100

    Rutas creadas para menu help y juego

---
+++ app/src/main/java/com/example/pr04_ahorcado/view/vistaHelp.kt
@@ -0,0 +1,7 @@
+package com.example.pr04_ahorcado.view
+
+sealed class Routes(val route: String) {
+    object Menu : Routes("vistaMenu.kt")
+    object Juego : Routes("vistaJuego.kt")
+    object Help : Routes("vistaHelp")
+}
