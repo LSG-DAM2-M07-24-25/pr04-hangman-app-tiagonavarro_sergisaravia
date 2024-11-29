d1679af2af44d59e110e3601f2ac35d64f03b4be
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2024-11-29 12:34:01 +0100

    funcion entryPoint creada

--- app/src/main/java/com/example/pr04_ahorcado/entryPoint.kt
+++ app/src/main/java/com/example/pr04_ahorcado/entryPoint.kt
@@ -4,7 +4,21 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
+import androidx.navigation.R
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import com.example.pr04_ahorcado.Routes
+import com.example.pr04_ahorcado.view.Routes
+
+@Composable
+
+fun entryPoint(navigationController: NavController) {
+    NavHost(
+        navController = navigationController as NavHostController,
+        startDestination = Routes.Menu.route
+    ){
+        composable(Routes.Menu.route) { Routes.Menu(navigationController) }
+        composable(Routes.Menu.route) { Routes.Help(navigationController) }
+        composable(Routes.Juego.route){ Routes.Juego(navigationController)}
+    }
+}
--- app/src/main/java/com/example/pr04_ahorcado/view/vistaHelp.kt
+++ app/src/main/java/com/example/pr04_ahorcado/view/vistaHelp.kt
@@ -1,7 +1,2 @@
 package com.example.pr04_ahorcado.view
 
-sealed class Routes(val route: String) {
-    object Menu : Routes("vistaMenu.kt")
-    object Juego : Routes("vistaJuego.kt")
-    object Help : Routes("vistaHelp")
-}
