74038a769e76c8ac9581e20464a6d3fc9d3c74ec
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2024-12-04 12:05:11 +0100

    correct: import que estaba dando problemas borrado en el entryPoint

--- app/src/main/java/com/example/pr04_ahorcado/view/entryPoint.kt
+++ app/src/main/java/com/example/pr04_ahorcado/view/entryPoint.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
-import androidx.navigation.R
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
