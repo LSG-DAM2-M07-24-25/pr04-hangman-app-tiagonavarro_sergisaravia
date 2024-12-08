8e4d8d377f2f16182265fbd930edab46f236b09a
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2024-12-08 18:42:14 +0100

    variables añadidas al modelo

--- app/src/main/java/com/example/pr04_ahorcado/model/Menu.kt
+++ app/src/main/java/com/example/pr04_ahorcado/model/Menu.kt
@@ -1,11 +1,24 @@
 package com.example.pr04_ahorcado.model
 
 data class ahorcado(
-    var dificultad: Int
-)
-val wordsList = arrayOf(
-    "CASA", "GAT", "GOS", "TAULA", "CADIRA", "COTXE", "FLOR", "NÚVOL", "MUNTANYA", "PLATJA",
-    "MAR", "AIGUA", "SOL", "LLUNA", "ESTRELLA", "CARRER", "ESCOLA", "LLIBRE", "BOLÍGRAF",
-    "TREBALL", "AMISTAT", "FESTA", "MÚSICA", "JARDÍ", "FAMÍLIA", "CIUTAT", "TREN", "AVENTURA",
-    "MISTERI", "POEMA", "PLANETA", "PAÍS", "CASTELL", "GUITARRA", "CINEMA", "TEATRE"
-)
+    var dificultad: Int,
+    val wordsList: List<String> = listOf(
+        "CASA", "GAT", "GOS", "TAULA", "CADIRA", "COTXE", "FLOR", "NÚVOL", "MUNTANYA", "PLATJA",
+        "MAR", "AIGUA", "SOL", "LLUNA", "ESTRELLA", "CARRER", "ESCOLA", "LLIBRE", "BOLÍGRAF",
+        "TREBALL", "AMISTAT", "FESTA", "MÚSICA", "JARDÍ", "FAMÍLIA", "CIUTAT", "TREN", "AVENTURA",
+        "MISTERI", "POEMA", "PLANETA", "PAÍS", "CASTELL", "GUITARRA", "CINEMA", "TEATRE"
+    ),
+    val maxAttemts: Int = 6
+){
+    private lateinit var currentWord: String
+    private val selectedKeys: MutableSet<Char> = mutableListOf()
+    private var attemptsLeft: Int = maxAttemts
+    private var gameOver: Boolean = false
+    private var win: Boolean = false
+
+    fun resetGame() {
+        currentWord = wordsList.random()
+        currentWord
+    }
+}
+
