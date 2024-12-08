247a5e58c86e5df27cb761e956dc732520cffc33
tiagunskii@gmail.com <tiagunskiii@gmail.com>
2024-12-08 17:39:06 +0100

    Array de words añadida al model

--- app/src/main/java/com/example/pr04_ahorcado/model/Menu.kt
+++ app/src/main/java/com/example/pr04_ahorcado/model/Menu.kt
@@ -3,3 +3,9 @@ package com.example.pr04_ahorcado.model
 data class ahorcado(
     var dificultad: Int
 )
+val wordsList = arrayOf(
+    "CASA", "GAT", "GOS", "TAULA", "CADIRA", "COTXE", "FLOR", "NÚVOL", "MUNTANYA", "PLATJA",
+    "MAR", "AIGUA", "SOL", "LLUNA", "ESTRELLA", "CARRER", "ESCOLA", "LLIBRE", "BOLÍGRAF",
+    "TREBALL", "AMISTAT", "FESTA", "MÚSICA", "JARDÍ", "FAMÍLIA", "CIUTAT", "TREN", "AVENTURA",
+    "MISTERI", "POEMA", "PLANETA", "PAÍS", "CASTELL", "GUITARRA", "CINEMA", "TEATRE"
+)
